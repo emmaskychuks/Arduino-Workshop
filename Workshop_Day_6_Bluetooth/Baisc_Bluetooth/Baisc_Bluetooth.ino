3860672ea8ed452ef7045b0e92cc95b5aaa92211
Michael Bowyer <mbowyer@umich.edu>
2016-12-01 10:53:43 -0500

    Arduino Workshop Day 6
    Let's get our robots ready for rocket league!

---
+++ Workshop_Day_6_Bluetooth/Baisc_Bluetooth/Baisc_Bluetooth.ino
@@ -0,0 +1,59 @@
+    /* JY-MCU Bluetooth Wireless Serial Port module for Arduino®
+    
+    ON and OFF Control of a LED thru Bluetooth .
+    The code example will switch ON or OFF on board LED by sending command ‘B’
+    or any other character thru the Bluetooth paired PC or Laptop.
+    
+    The pins used are designed are for JY-MCU Bluetooth Wireless Serial Port
+    module with Arduino® Uno Board available from:
+    
+  https://core-electronics.com.au/attachments/guides/Product-User-Guide-JY-MCU-Bluetooth-UART-R1-0.pdf
+    
+    This example code is in the public domain.
+    
+    */ 
+    // Declaration of constants and variable to used by program
+    
+    
+    char recd_dat; // variable for receiving data from bluetooth serial port
+    int on_brd_led = 13; // On-board LED pin detail
+    
+    void setup()
+    {
+        // initialize the serial communications:
+        // serial communication is used to receive the data from
+        // Bluetooth module
+        
+        Serial.begin(9600);//start serial communication  so the arduino will communicate over Serial
+        //the key here is that the arduino is no longer communicating with a PC using Serial communication
+        //It is now communicating with the bluetooth module using Serial. The bluetooth module recieves bluetooth
+        //commands from another bluetooth device (IE a mobile phone) interprets the commands, and sends them 
+        //to the arduino over serial.
+        
+        // Onboard LED pin as output
+        pinMode(on_brd_led, OUTPUT);
+        
+        // The initial state of led is defined here.
+        // HIGH on PIN will switch on the LED
+        // LOW on PIN will switch off the LED
+        digitalWrite(on_brd_led, LOW);
+    }
+    
+    void loop() 
+    {
+       if( Serial.available() ) // if serial data is available to read 
+       {
+           recd_dat = Serial.read(); //read data & store it in ‘recd_dat’ 
+       }
+       if(recd_dat == 'B' ) // if 'B' was received
+       {
+           digitalWrite(on_brd_led, HIGH); // turn ON LED
+        }
+        else
+        {
+           digitalWrite(on_brd_led, LOW); // otherwise switch OFF 
+        } 
+        delay(10); 
+        // Just wait 150ms for next reading 
+    }
+      
---
+++ Workshop_Day_6_Bluetooth/Motor_Control_With_Bluetooth/Motor_Control_With_Bluetooth.ino
@@ -0,0 +1,160 @@
+/*
+* Arduino code for SN754410 H-bridge
+* motor driver control using HC-06 Bluetooth communication Module!
+*Control the speed of the motor using A bluetooth app!
+*Created by: Michael Bowyer 11/23/2016
+
+//this code works best with the google play app titled "Android bluetooth controller"
+//it is on the google play store at 
+
+// https://play.google.com/store/apps/details?id=com.giumig.apps.bluetoothserialmonitor&hl=en
+
+//this can be used in controller mode.
+*/
+
+//H-Bridge Pin Information to control Left motor
+int motorL_speedPin = 11; // Speed(PWM) control H-bridge output 
+int motorL_Forward = 12; // H-bridge direction control 
+int motorL_Backward = 13; // H-bridge direction control 
+
+//H-Bridge Pin Information to control Right motor
+int motorR_speedPin = 5; // Speed(PWM) control H-bridge output 
+int motorR_Forward = 6; // H-bridge direction control 
+int motorR_Backward = 7; // H-bridge direction control 
+
+//Variables needed to control motor
+float left_motor_speed=0; // value for motor speed
+float right_motor_speed=0; // value for motor speed
+int start_stop=0;
+
+//BLUETOOTH variables to read in from the bluetooth module
+char recieved_data; // variable for receiving data from bluetooth serial port
+int LED = 13; // On-board LED pin detail
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600); //note that the bluetooth module will be communicating with the arduino using serial!
+  
+  //set left motor interface pins as outputs
+   pinMode(motorL_speedPin, OUTPUT);// set Speed(PWM) control H-bridge as output 
+   pinMode(motorL_Forward, OUTPUT);// H-bridge direction control as output
+   pinMode(motorL_Backward, OUTPUT);// H-bridge direction control as output
+
+//set right motor interface pins as outputs
+   pinMode(motorR_speedPin, OUTPUT);// set Speed(PWM) control H-bridge as output 
+   pinMode(motorR_Forward, OUTPUT);// H-bridge direction control as output
+   pinMode(motorR_Backward, OUTPUT);// H-bridge direction control as output
+  
+//initializing left motor speed and direction
+  analogWrite(motorL_speedPin, 0);//set initial speed of left to 0
+  digitalWrite(motorL_Forward, HIGH);//have left motor drive forward.
+  digitalWrite(motorL_Backward, LOW);//have left motor drive forward. 
+//initializing right motor speed and direction  
+  analogWrite(motorR_speedPin, 0);//set initial speed of right to 0
+  digitalWrite(motorR_Forward, HIGH);//have right motor drive forward.
+  digitalWrite(motorR_Backward, LOW);//have right motor drive forward. 
+  
+  //initializng bluetooth controlled LED output
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+}
+
+void loop() {
+  //start out by turning motors off if no input is recieved
+  left_motor_speed=0; // calculate speed to send to left motor
+  right_motor_speed= 0;//calculate speed to send to right motor
+
+//Gather Joystick and Button Push Values
+   if( Serial.available() ) // if serial data is available to read 
+   {
+     recieved_data = Serial.read(); //read data & store it in ‘recd_dat’ 
+   }
+
+//Print out Joystick Data which was read
+    Serial.print("Recieved from Bluetooth: ");
+    Serial.print(recieved_data);
+        
+//CALCULATE SPEED OF MOTORS BASED ON INPUT
+switch (recieved_data) {
+    case 'W'://Drive Robot Forward
+       left_motor_speed=254;// Turn left motor full speed
+       right_motor_speed= 254;//turn right motor full speed
+      break;
+    case 'A'://turn robot left
+      left_motor_speed=-254;// Turn left motor full speed
+      right_motor_speed= 254;//turn right motor full speed
+      break;
+    case 'S'://drive robot backward
+      left_motor_speed=-254;// Turn left motor full speed
+      right_motor_speed=-254;//turn right motor full speed
+      break;
+    case 'D'://turn robot right
+      left_motor_speed=254;// Turn left motor full speed
+      right_motor_speed=-254;//turn right motor full speed
+      break; 
+    case 'Z'://Force Robot to Stop      left_motor_speed=254;// Turn left motor full speed
+      start_stop=0;
+      break; 
+    case 'X'://Allow Robot to Move again      left_motor_speed=254;// Turn left motor full speed
+      start_stop=1;
+      break;   
+    default:  // if nothing else matches, do the default
+     left_motor_speed=0; // calculate speed to send to left motor
+     right_motor_speed= 0;//calculate speed to send to right motor
+    break;
+}
+
+//write the speeds to the motors!
+  if(start_stop==1)//if "start" was pressed on the controller, we don't want the vehicle to move. This means start_stop ==1
+  {
+    sendMotorSpeed(left_motor_speed, right_motor_speed);//send the speeds from the controller. 
+  }
+  else//if "select" was pressed on the controller, we don't want the vehicle to move. This means start_stop ==0
+  {
+    sendMotorSpeed(0, 0);// stop the vehicle
+  }
+  
+  delay(10); // add some delay between reads
+}
+
+
+void sendMotorSpeed(float lSpeed, float rSpeed)//change direction of motors depending on input value
+{
+  //saving values to send to speed as integers, instead of floats
+  int leftSpeed= (int)lSpeed;
+  int rightSpeed= (int)rSpeed;
+  
+  if( leftSpeed < 0 )//if left motor needs to go backward
+  {
+       digitalWrite(motorL_Forward, LOW);//have left motor drive backward.
+       digitalWrite(motorL_Backward, HIGH);//have left motor drive backward. 
+       analogWrite(motorL_speedPin, abs(leftSpeed));//change speed of left motor
+       Serial.print("   LMS BACKWARD: ");//print left motor speed
+       Serial.print(left_motor_speed);
+  }
+  else //if left motor needs to go forward
+  {
+        digitalWrite(motorL_Forward, HIGH);//have left motor drive forward.
+       digitalWrite(motorL_Backward, LOW);//have left motor drive forward. 
+       analogWrite(motorL_speedPin, abs(leftSpeed));//change speed of left motor
+       Serial.print("   LMS FORWARD: ");//print left motor speed
+       Serial.print(left_motor_speed);
+  }
+  if( rightSpeed < 0 )//if right motor needs to go backward
+  {
+       digitalWrite(motorR_Forward, LOW);//have right motor drive backward.
+       digitalWrite(motorR_Backward, HIGH);//have right motor drive backward. 
+       analogWrite(motorR_speedPin, abs(rightSpeed));//change speed of right motor
+       Serial.print("   RMS BACKWARD: ");//print left motor speed
+       Serial.println(right_motor_speed);
+  }
+  else//if left motor needs to go forward 
+  {
+        digitalWrite(motorR_Forward, HIGH);//have right motor drive forward.
+       digitalWrite(motorR_Backward, LOW);//have right motor drive forward. 
+       analogWrite(motorR_speedPin, abs(rightSpeed));//change speed of right motor
+       Serial.print("   RMS FORWARD: ");//print left motor speed
+       Serial.println(right_motor_speed);
+  }
+  return;
+}
